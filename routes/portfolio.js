743f43e50a89034a9b30d25a224376574802cb35
ashudeo4 <ashudeo4@gmail.com>
2021-05-05 13:24:26 +0530

    refactor: comments

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -8,10 +8,14 @@ const {
 } = require("../controllers/portfolio");
 
 const { addPortfolioValidation } = require("../utils/validation");
+
+//For adding portfolio
 router.post("/", addPortfolioValidation, addPortfolio);
 
+//For getting the portfolio
 router.get("/", getPortfolio);
 
+//For getting the returns
 router.get("/returns", getReturns);
 
 module.exports = router;
--- routes/trade.js
+++ routes/trade.js
@@ -16,14 +16,19 @@ const {
   tradeUpdateValidation,
 } = require("../utils/validation");
 
+//For getting the trades
 router.get("/", getAllTrades);
 
+//for buying shares
 router.post("/buy", tradeBuyValidation, buyShares);
 
+//for selling shares
 router.post("/sell", tradeSellValidation, sellShares);
 
+//for deleting a specific trade
 router.delete("/", tradeRemoveValidation, removeTrade);
 
+//for updating the trade
 router.put("/", tradeUpdateValidation, updateTrade);
 
 module.exports = router;
